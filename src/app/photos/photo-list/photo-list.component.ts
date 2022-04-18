9e1bd9dd66ae7dc912fde7eb4f0a2671a5fb4ed0
diegosampaio <diego.estaleiro@gmail.com>
2022-04-18 18:42:19 -0300

    Resolver de listagens

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
-
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
@@ -13,13 +11,10 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
 
-  constructor(private photoService: PhotoService,
-              private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params['userName'];
-    this.photoService.listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 
   observavelFiltro(termo: string) {
