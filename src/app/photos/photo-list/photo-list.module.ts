415d559fa1feff6b53ff45f5488918711e862829
diegosampaio <diego.estaleiro@gmail.com>
2022-04-19 11:03:07 -0300

    Organizando projeto em sub-modulos

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,23 @@
+import { PhotoModule } from './../photo/photo.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoListComponent } from './photo-list.component';
+import { FilterByDescription } from './filter-by-description.pipe';
+
+@NgModule({
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescription
+  ],
+  imports: [
+    CommonModule,
+    PhotoModule
+  ]
+})
+
+export class PhotoListModule {}
